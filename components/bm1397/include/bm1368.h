57d79ca50250bfe8b611a60abe76becef8d8167f
cnc <cnc@debian-i7>
2024-04-15 20:10:12 -0400

    datatypes fix

--- components/bm1397/include/bm1368.h
+++ components/bm1397/include/bm1368.h
@@ -8,7 +8,7 @@
 #define CRC5_MASK 0x1F
 
 // static const u_int64_t BM1368_FREQUENCY = CONFIG_ASIC_FREQUENCY;
-static const u_int64_t BM1368_CORE_COUNT = 672;
+static const uint64_t BM1368_CORE_COUNT = 672;
 // static const u_int64_t BM1368_HASHRATE_S = BM1368_FREQUENCY * BM1368_CORE_COUNT * 1000000;
 //  2^32
 //  static const u_int64_t NONCE_SPACE = 4294967296;
@@ -31,7 +31,7 @@ typedef struct __attribute__((__packed__))
     uint8_t version[4];
 } BM1368_job;
 
-void BM1368_init(u_int64_t frequency);
+void BM1368_init(uint64_t frequency);
 
 void BM1368_send_init(void);
 void BM1368_send_work(void * GLOBAL_STATE, bm_job * next_bm_job);
